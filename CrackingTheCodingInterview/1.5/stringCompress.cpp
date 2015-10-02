d51b46a03fc525f28b497b76f56ac9f52ac9cf9a
Alvin Hung <ahung93@gmail.com>
2015-10-01 20:42:12 -0700

    CrackingTheCodingInterview 1.5: Created solution for 1.5

--- CrackingTheCodingInterview/1.5/stringCompress.cpp
+++ CrackingTheCodingInterview/1.5/stringCompress.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
 string strCompress(string str);
@@ -16,16 +16,45 @@ string strCompress(string str){
 	int charCount = 1;
 	int y = 0;
 	char currentChar = str[0];
-	char *buffer = new char[5000];
-    string digitCount;
-    string newString(str.size()-1, ' ');
+	string digitCount;
+    string newString(str.size(), 'b');
+    newString[y++] = currentChar;
+
     for(int i = 1; i < str.size(); i++){
     	if(currentChar == str[i]){
     		charCount++;
     	} else {
-    		digitCount = itoa(charCount, buffer, 10);
+    		digitCount = to_string(charCount);
+
+            // Check if it exceeds regular string
     		if((y + digitCount.size()) >= str.size())
     			return str;
+
+            // Place the string representation into compact
+            for(int j = 0; j < digitCount.size(); j++){
+                newString[y++] = digitCount[j];
+            }
+
+            // Reset counter and character
+            charCount = 1;
+            currentChar = str[i];
+            newString[y++] = currentChar;
     	}
     }
+
+    /* If we have left for loop, then we need
+    **   to place charCount into new string array again
+    */
+    digitCount = to_string(charCount);
+
+    // Check if it exceeds regular string
+    if((y + digitCount.size()) >= str.size())
+        return str;
+
+    // Place the string representation into compact
+    for(int j = 0; j < digitCount.size(); j++){
+        newString[y++] = digitCount[j];
+    }
+    newString.resize(y);
+    return newString;
 }
